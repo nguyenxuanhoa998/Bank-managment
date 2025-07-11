d1d24023ce5e5627e2ab0f012994329bec2a68dd
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-07-11 23:39:47 +0700

    create a new frame for log in

--- src/bank/managment/system/Login.java
+++ src/bank/managment/system/Login.java
@@ -3,7 +3,11 @@ package bank.managment.system;
 import javax.swing.*;
 
 public class Login extends JFrame {
+    Login(){
+        setSize(800, 400);
+        setVisible(true);
+    }
     public static void main (String [] args ){
-        new
+        new Login();
     }
 }

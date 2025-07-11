59c28bb5a1afa719de50b34fa39b6b56fa048157
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-07-11 23:47:33 +0700

    a title for the app

--- src/bank/managment/system/Login.java
+++ src/bank/managment/system/Login.java
@@ -3,9 +3,12 @@ package bank.managment.system;
 import javax.swing.*;
 
 public class Login extends JFrame {
+    
     Login(){
+        setTitle("AUTOMATED TELLER MACHINE");
         setSize(800, 400);
         setVisible(true);
+        setLocation(400, 200);
     }
     public static void main (String [] args ){
         new Login();

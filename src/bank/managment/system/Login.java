e032cbb52166d6f945a259a83adda83379fb015d
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-07-12 23:18:40 +0700

    more icon

--- src/bank/managment/system/Login.java
+++ src/bank/managment/system/Login.java
@@ -1,12 +1,14 @@
 package bank.managment.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame {
     
     Login(){
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/thumbnail-logo-BIDV.jpg"));
-        
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3= new JLabel(i2);
         setTitle("AUTOMATED TELLER MACHINE");
         setSize(800, 400);
         setVisible(true);

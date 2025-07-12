88aeb63425c779dcb540b2e0d344f997fd3d4837
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-07-12 23:09:37 +0700

    BIDV BANK ICON

--- src/bank/managment/system/Login.java
+++ src/bank/managment/system/Login.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class Login extends JFrame {
     
     Login(){
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource())
         setTitle("AUTOMATED TELLER MACHINE");
         setSize(800, 400);
         setVisible(true);

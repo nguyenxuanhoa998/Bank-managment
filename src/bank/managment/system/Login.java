d2d063c11f89f5880542c9278c8d3ad1043c9ab1
nguyenxuanhoa998 <nguyenxuanhoa223@gmail.com>
2025-07-13 23:26:45 +0700

    ome change in icon

--- src/bank/managment/system/Login.java
+++ src/bank/managment/system/Login.java
@@ -4,17 +4,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Login extends JFrame {
-    
-    Login(){
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/thumbnail-logo-BIDV.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
-        ImageIcon i3= new JLabel(i2);
+
+    Login() {
+        // Đặt tiêu đề và thông số khởi đầu
         setTitle("AUTOMATED TELLER MACHINE");
         setSize(800, 400);
-        setVisible(true);
         setLocation(400, 200);
+        setLayout(null); // Rất quan trọng khi dùng setBounds
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Load hình ảnh từ thư mục package-icons
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("package-icons/thumbnail-logo-BIDV.jpg"));
+
+        // Kiểm tra ảnh có load được không
+        if (i1.getImageLoadStatus() != MediaTracker.COMPLETE) {
+            System.out.println("❌ Không tìm thấy hoặc không load được ảnh.");
+        }
+
+        // Scale và đặt vào JLabel
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel(i3);
+        label.setBounds(20, 20, 100, 100);
+        add(label);
+
+        setVisible(true);
     }
-    public static void main (String [] args ){
+
+    public static void main(String[] args) {
         new Login();
     }
 }
